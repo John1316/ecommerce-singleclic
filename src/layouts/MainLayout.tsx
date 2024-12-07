5c3c42bab082f908afba66a8363698554461ec72
John1316 <johnmaher179@gmail.com>
2024-12-07 18:17:40 +0200

    add cart and icons with reusable components

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Header from '../ui/components/layout/Header'
 import Footer from '../ui/components/layout/Footer'
 import { Outlet } from 'react-router-dom'
--- src/ui/components/Cart/ProductCardCart.tsx
+++ src/ui/components/Cart/ProductCardCart.tsx
@@ -1,4 +1,6 @@
 import { useCart } from "../../../hooks/useCart"
+import MinusCartIcon from "../../svgs/MinusCartIcon"
+import PlusCartIcon from "../../svgs/PlusCartIcon"
 
 export default function ProductCardCart({
     product
@@ -16,15 +18,11 @@ export default function ProductCardCart({
                 <div className="flex items-center justify-between md:order-3 md:justify-end">
                     <div className="flex items-center">
                         <button type="button" id="decrement-button" data-input-counter-decrement="counter-input" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                            <svg className="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
-                            </svg>
+                            <MinusCartIcon />
                         </button>
                         <input type="text" id="counter-input" data-input-counter className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white" placeholder="" value={product.quantity} required />
                         <button type="button" id="increment-button" data-input-counter-increment="counter-input" className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                            <svg className="h-2.5 w-2.5 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                            </svg>
+                            <PlusCartIcon />
                         </button>
                     </div>
                     <div className="text-end md:order-4 md:w-32">
