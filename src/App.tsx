55cfc66a23e3797413176b746cf52b57f6f9ba4c
John1316 <johnmaher179@gmail.com>
2024-12-07 23:03:47 +0200

    add fix bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-// import MainLayout from './layouts/MainLayout';
-// import Products from './pages/Products/Index';
+
 import routes from './config/routes';
 import './styles/style.scss'
-// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import {  createHashRouter, RouterProvider } from "react-router-dom";
 
 function App() {
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import Provider from './Provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
